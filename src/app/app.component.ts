941ec14d2d41dd19caa48decdcd85cd67845b747
saurabh siwa <saurabh.siwa@quantiphi.com>
2018-10-04 01:48:03 +0530

    footerandheadersametime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  host: {
+    '(window:scroll)': 'onScroll($event)'
+  }
 })
 export class AppComponent {
-  title = 'Project1';
+  isScrolled = false;
+  isScrolled1 = false;
 
-  constructor(private router: Router){}
+  currPos: Number = 0;
+  startPos: Number = 0;
+  changePos: Number = 2;
 
-  ngOnInit(){
-      this.router.navigate(['Login']);
+  constructor(private router: Router) { }
+
+  onScroll(evt) {//window object can be wrapper in a service but for now we directly use it
+    this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+    if (this.currPos >= this.changePos) {
+      console.log("is scrolled is set to true" + this.currPos);
+
+
+
+      if (this.currPos >= 20) {
+        this.isScrolled = true;
+        this.isScrolled1 = true;
+
+      } else {
+
+      }
+
+
+
+    } else {
+      console.log("is scrolled is set to false");
+      this.isScrolled = false;
+      this.isScrolled1 =false;
+
+
+    }
   }
-  
+
+  ngOnInit() {
+    this.router.navigate(['Login']);
+  }
+
 
 }
